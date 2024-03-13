7b4a9f2d7e70ba951383c0e7c5ed987a48fdbd2e
Topsidegem <smendez@cocoschool.mx>
2024-03-12 20:33:37 -0600

    Creacion del proyecto y Bola y Barra
    Quedan errores pero se hizo el codigo de la bola y la barra

---
+++ Assets/Scripts/PlayerBar.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBar : MonoBehaviour
+{
+    private Player controls; 
+    public float speed = 5f; 
+    private float moveInput;
+
+
+    void Start()
+    {
+        controls = new Player();
+        controls.Enable();
+
+        //preguntar como funciana este codigo de chatgpt
+        controls.Movement.Derecha.performed += ctx =>
+        {
+            moveInput = ctx.ReadValue<float>();
+        };
+        controls.Movement.Derecha.canceled += ctx => moveInput = 0f;
+
+        controls.Movement.Izquierda.performed += ctx =>
+        {
+            moveInput = -ctx.ReadValue<float>(); 
+        };
+        controls.Movement.Izquierda.canceled += ctx => moveInput = 0f;
+    }
+
+    void FixedUpdate()
+    {
+        if (moveInput != 0)
+        {
+            Vector2 movement = (moveInput > 0) ? Vector2.right : Vector2.left;
+            transform.Translate(movement * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<float>();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+}
+
+
